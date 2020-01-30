b1b1741c2c3036a923aa4c2485d029178658312d
stoically <stoically@protonmail.com>
2020-01-30 15:54:50 +0100

    WebExtensions support

--- src/vector/app.js
+++ src/vector/app.js
@@ -41,6 +41,7 @@ import {parseQs, parseQsFromFragment} from './url_utils';
 
 import ElectronPlatform from './platform/ElectronPlatform';
 import WebPlatform from './platform/WebPlatform';
+import WebExtensionPlatform from './platform/WebExtensionPlatform';
 
 import {MatrixClientPeg} from 'matrix-react-sdk/src/MatrixClientPeg';
 import SettingsStore from "matrix-react-sdk/src/settings/SettingsStore";
@@ -195,6 +196,10 @@ export async function loadApp() {
         console.log("Using Electron platform");
         const plaf = new ElectronPlatform();
         PlatformPeg.set(plaf);
+    } else if (window.browser && window.browser.runtime) {
+        console.log("Using WebExtension platform");
+        const plaf = new WebExtensionPlatform();
+        PlatformPeg.set(plaf);
     } else {
         console.log("Using Web platform");
         PlatformPeg.set(new WebPlatform());
@@ -274,7 +279,7 @@ export async function loadApp() {
         return;
     }
 
-    const validBrowser = checkBrowserFeatures();
+    const validBrowser = platform.forceValidBrowser() || checkBrowserFeatures();
 
     const acceptInvalidBrowser = window.localStorage && window.localStorage.getItem('mx_accepts_unsupported_browser');
 
